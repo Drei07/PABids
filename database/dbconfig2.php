12aaf00337d7d32d495ff156d2ff5575d85e23ca
ANDREI VISCAYNO <2018006164@dhvsu.edu.ph>
2023-10-12 22:16:32 +0800

    DATABASE

--- database/dbconfig2.php
+++ database/dbconfig2.php
@@ -4,11 +4,11 @@
 	try {
 
 		// localhost
-		$pdoConnect = new PDO("mysql:host=localhost;dbname=pabids", "root", "");
+		// $pdoConnect = new PDO("mysql:host=localhost;dbname=pabids", "root", "");
 
 		// Live
-		// $pdoConnect = new PDO("mysql:host=localhost;dbname=u297724503_ecket", "u297724503_ecket", "E-cket@2023");
-		// $pdoConnect->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+		$pdoConnect = new PDO("mysql:host=localhost;dbname=u297724503_pabids", "u297724503_pabids", "Pabids_2023$");
+		$pdoConnect->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
 
 	}
 	catch (PDOException $exc){
